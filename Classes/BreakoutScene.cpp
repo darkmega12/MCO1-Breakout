b1114e2572595a2dcc1d8c1b43b553a2b2849019
Kiel <christian_abejuro@yahoo.com.ph>
2016-02-27 11:00:59 +0800

    Basic Functionalities working, no physics

--- Classes/BreakoutScene.cpp
+++ Classes/BreakoutScene.cpp
@@ -17,12 +17,15 @@ Scene* BreakoutScene::createScene()
 	return scene;
 }
 
-Label* scoreValue;
+
+
 
 void BreakoutScene::initEnvironment()
 {
-	
+	///////////////
 	// Code for creating the borders
+	//
+	//////////////
 	auto leftWallSprite = Sprite::create("wall_vertical.png");
 	leftWallSprite->setAnchorPoint(Vec2());
 	leftWallSprite->setPosition(0, 100);
@@ -47,6 +50,10 @@ void BreakoutScene::initEnvironment()
 
 	this->addChild(bottomWallSprite, 0);
 
+	///////////////
+	// code for label and positioning
+	//
+	//////////////
 	auto lifeLabel = Label::createWithTTF("Lives: ", "fonts/Marker Felt.ttf", 24);
 	lifeLabel->setAnchorPoint(Vec2());
 	lifeLabel->setPosition(10, 0);
@@ -59,11 +66,65 @@ void BreakoutScene::initEnvironment()
 
 	this->addChild(scoreLabel, 1);
 
-	scoreValue = Label::createWithTTF("0", "fonts/Marker Felt.ttf", 24);
+	scoreValue = Label::createWithTTF(std::to_string(score), "fonts/Marker Felt.ttf", 24);
 	scoreValue->setAnchorPoint(Vec2());
 	scoreValue->setPosition(600, 0);
 
 	this->addChild(scoreValue, 2);
+	
+	pLives = CCArray::create();
+	Sprite *life = Sprite::create("ball.png");
+	life->setAnchorPoint(Vec2());
+	life->setPosition(80, 0);
+	this->addChild(life);
+	pLives->addObject(life);
+
+	life = Sprite::create("ball.png");
+	life->setAnchorPoint(Vec2());
+	life->setPosition(120, 0);
+	this->addChild(life);
+	pLives->addObject(life);
+
+	life = Sprite::create("ball.png");
+	life->setAnchorPoint(Vec2());
+	life->setPosition(160, 0);
+	this->addChild(life);
+	pLives->addObject(life);
+
+	///////////////
+	//The following section is for creating/initializing the blocks to break
+	//
+	//////////////
+
+	int numRows = 5;
+	int numColumns = 10;
+	int brickWidth = 80;
+	int brickLength = 25;
+	Sprite* brick;
+
+	for (int i = 0; i < numRows; i++)
+	{
+		for (int j = 0; j < numColumns; j++)
+		{
+			switch (i)
+			{
+				case 0:
+				case 2:
+					brick = Sprite::create("redbrick.png");
+					break;
+				case 1:
+				case 4:
+					brick = Sprite::create("bluebrick.png");
+					break;
+				case 3:
+					brick = Sprite::create("orangebrick.png");
+					break;
+			}
+			brick->setAnchorPoint(Vec2());
+			brick->setPosition(20 + (5 * (j + 1)) + (j * brickWidth), 605 - (i * brickLength) - (4 * i)); //handles the spacing for the bricks' position
+			this->addChild(brick, 10);
+		}
+	}
 }
 
 // on "init" you need to initialize your instance
@@ -79,11 +140,7 @@ bool BreakoutScene::init()
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-	/////////////////////////////
-	// 2. add a menu item with "X" image, which is clicked to quit the program
-	//    you may modify it.
-
-	// add a "close" icon to exit the progress. it's an autorelease object
+	// adds a "close" icon to exit the progress. it's an autorelease object
 	auto closeItem = MenuItemImage::create(
 		"CloseNormal.png",
 		"CloseSelected.png",
@@ -97,38 +154,123 @@ bool BreakoutScene::init()
 	menu->setPosition(Vec2::ZERO);
 	this->addChild(menu, 1);
 
-	/*
-	/////////////////////////////
-	// 3. add your codes below...
-
-	// add a label shows "Hello World"
-	// create and initialize a label
-
-	auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-
-	// position the label on the center of the screen
-	label->setPosition(Vec2(origin.x + visibleSize.width / 2,
-		origin.y + visibleSize.height - label->getContentSize().height));
-
-	// add the label as a child to this layer
-	this->addChild(label, 1);
-
-	// add "HelloWorld" splash screen"
-	auto sprite = Sprite::create("HelloWorld.png");
-
-	// position the sprite on the center of the screen
-	sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
-
-	// add the sprite as a child to this layer
-	this->addChild(sprite, 0);
-	*/
+	//initializing the variables
+	//separated the initialization of the breaker and mrBall from the rest of the assets
+	_bricks = CCArray::create();
+	score = 0;
 
 	initEnvironment();
 
+	breaker = Sprite::create("breaker.png");
+	breaker->setPosition(450, 170);
+	this->addChild(breaker, 10);
+
+	mrBall = Sprite::create("ball.png");
+	mrBall->setPosition(450, 300);
+	this->addChild(mrBall, 10);
+	move_horizontal = horizontalDirection::LEFT;
+	move_vertical = verticalDirection::DOWN;
+
+	isLeftPressed = false;
+	isRightPressed = false;
+	isPaused = true;
+
+	auto keyListener = EventListenerKeyboard::create();
+	keyListener->onKeyPressed = [=](EventKeyboard::KeyCode code, Event *event)->void{
+		Vec2 currPos = breaker->getPosition();
+		switch (code)
+		{
+			case EventKeyboard::KeyCode::KEY_LEFT_ARROW:
+			case EventKeyboard::KeyCode::KEY_A:
+				isLeftPressed = true;
+				breaker->setPosition(currPos.x - 5, currPos.y);
+				break;
+			case EventKeyboard::KeyCode::KEY_RIGHT_ARROW:
+			case EventKeyboard::KeyCode::KEY_D:
+				isRightPressed = true;
+				breaker->setPosition(currPos.x + 5, currPos.y);
+				break;
+			case EventKeyboard::KeyCode::KEY_SPACE:
+				if (isPaused)
+					isPaused = false;
+				else
+					isPaused = true;
+		}
+	};
+
+	keyListener->onKeyReleased = [=](EventKeyboard::KeyCode code, Event *event) {
+		switch (code)
+		{
+			case EventKeyboard::KeyCode::KEY_LEFT_ARROW:
+			case EventKeyboard::KeyCode::KEY_A:
+				isLeftPressed = false;
+				break;
+			case EventKeyboard::KeyCode::KEY_RIGHT_ARROW:
+			case EventKeyboard::KeyCode::KEY_D:
+				isRightPressed = false;
+				break;
+		}
+	};
+
+	this->_eventDispatcher->addEventListenerWithSceneGraphPriority(keyListener, this);
+
+	this->scheduleUpdate();
 	return true;
 
 }
 
+void BreakoutScene::moveBall()
+{
+	Vec2 currPosition = mrBall->getPosition();
+	if (move_horizontal == horizontalDirection::LEFT)
+	{
+		if (move_vertical == verticalDirection::UP)
+		{
+			mrBall->setPosition(currPosition.x - 1, currPosition.y + 1);
+		}
+		else if (move_vertical == verticalDirection::DOWN)
+		{
+			mrBall->setPosition(currPosition.x - 1, currPosition.y - 1);
+		}
+	}
+	else if (move_horizontal == horizontalDirection::RIGHT)
+	{
+		if (move_vertical == verticalDirection::UP)
+		{
+			mrBall->setPosition(currPosition.x + 1, currPosition.y + 1);
+		}
+		else if (move_vertical == verticalDirection::DOWN)
+		{
+			mrBall->setPosition(currPosition.x + 1, currPosition.y - 1);
+		}
+	}
+}
+
+void BreakoutScene::update(float delta)
+{
+	if (gameOver)
+	{
+		Label *gameOverLabel = Label::create("Game over", "fonts/Marker Felt.ttf", 30);
+		gameOverLabel->setAnchorPoint(Vec2());
+		gameOverLabel->setPosition(450, 310);
+
+		this->addChild(gameOverLabel, 20);
+	}
+	else if (!isPaused)
+	{
+		moveBall();
+
+		Vec2 currPos = breaker->getPosition();
+		if (isLeftPressed && currPos.x - 80 > 20)
+		{
+			breaker->setPosition(currPos.x - 5, currPos.y);
+		}
+		else if (isRightPressed && currPos.x + 80 < 880)
+		{
+			breaker->setPosition(currPos.x + 5, currPos.y);
+		}
+	}
+}
 
 void BreakoutScene::menuCloseCallback(Ref* pSender)
 {
--- Classes/BreakoutScene.h
+++ Classes/BreakoutScene.h
@@ -6,10 +6,36 @@ USING_NS_CC;
 
 class BreakoutScene : public cocos2d::Layer
 {
+private:
+	CCArray *pLives;
+	int move_horizontal;
+	int move_vertical;
+	int score;
+	Label* scoreValue;
+	Sprite *mrBall;
+	Sprite *breaker;
+
+	bool isLeftPressed;
+	bool isRightPressed;
+	bool isPaused;
+	bool gameOver = false;
+
+	struct horizontalDirection
+	{
+		enum type{ LEFT = 0, RIGHT = 1};
+	};
+	struct verticalDirection
+	{
+		enum type{ UP = 0, DOWN = 1};
+	};
+protected:
+	cocos2d::CCArray *_bricks;
+	cocos2d::CCArray *_ball;
 public:
 	static cocos2d::Scene* createScene();
 
 	virtual bool init();
+
 
 	//initialize the assets in their positions
 	void initEnvironment();
@@ -19,6 +45,10 @@ public:
 
 	// implement the "static create()" method manually
 	CREATE_FUNC(BreakoutScene);
+
+	void update(float) override;
+
+	void moveBall();
 
 	BreakoutScene();
 	~BreakoutScene();
